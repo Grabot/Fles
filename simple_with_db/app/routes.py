27a1d4682267d6a41385dedc81b8741cba617ad3
Sander Kools <sanderkools@gmail.com>
2019-11-07 16:25:47 +0100

    made a simple page as preparation for the database

--- simple_with_db/app/routes.py
+++ simple_with_db/app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mark'}
+    posts = [
+        {
+            'author': {'username': 'Frank'},
+            'body': 'Sander kan goed coden!'
+        },
+        {
+            'author': {'username': 'Ronald'},
+            'body': 'Sander maakt heel vaak ontzettend goeie grappen'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
