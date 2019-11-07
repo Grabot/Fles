da1c04eb943e784f75364f352bc5c4e0765d37f7
Sander Kools <sanderkools@gmail.com>
2019-11-07 15:17:23 +0100

    attempt to make a simple app with database

--- simple_with_db/app.py
+++ simple_with_db/app.py
@@ -1,23 +1,10 @@
-from flask import Flask, request
-
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
     return "Hello World!"
-
-
-@app.route("/name/<name>")
-def get_book_name(name):
-    return "name : {}".format(name)
-
-
-@app.route("/details")
-def get_book_details():
-    author=request.args.get('author')
-    published=request.args.get('published')
-    return "Author : {}, Published: {}".format(author,published)
 
 
 if __name__ == '__main__':
