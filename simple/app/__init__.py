590bb9905717f947022385f391624d0b44c0a7bd
Sander Kools <sanderkools@gmail.com>
2019-11-08 13:54:14 +0100

    login is required and redirects

--- simple/app/__init__.py
+++ simple/app/__init__.py
@@ -10,6 +10,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes
 from app import models
--- simple/app/routes.py
+++ simple/app/routes.py
@@ -2,8 +2,12 @@ from flask import render_template
 from flask import flash
 from flask import redirect
 from flask import url_for
+from flask import request
 from flask_login import current_user
 from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
+from werkzeug.urls import url_parse
 from app.forms import LoginForm
 from app.models import User
 from app import app
@@ -11,8 +15,8 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Sander'}
     posts = [
         {
             'author': {'username': 'Mark'},
@@ -23,7 +27,7 @@ def index():
             'body': 'Mark heeft het al gezegd, maar inderdaad Sander is slim!'
         }
     ]
-    return render_template('index.html', title='Grabot', user=user, posts=posts)
+    return render_template('index.html', title='Grabot', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -37,6 +41,15 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
