e6c9b73f5ec10533d59c56618dc2fceec92103b9
Sander Kools <sanderkools@gmail.com>
2019-11-08 13:08:56 +0100

    made a simple page and starting with database

--- simple/app/routes.py
+++ simple/app/routes.py
@@ -1,6 +1,31 @@
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Sander'}
+    posts = [
+        {
+            'author': {'username': 'Mark'},
+            'body': 'Sander is slim'
+        },
+        {
+            'author': {'username': 'Ronald'},
+            'body': 'Mark heeft het al gezegd, maar inderdaad Sander is slim!'
+        }
+    ]
+    return render_template('index.html', title='Grabot', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
